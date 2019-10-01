2cea1232d31008307bda3c0d359a4b6841f9b3e4
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2019-10-01 16:53:49 +0200

    Replace buffer loop with a cpal output buffer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,73 @@
+extern crate cpal;
+extern crate failure;
+
 use std::f32::consts::PI;
 
-const SAMPLE_RATE: f32 = 44_100.0;
+use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
 
-fn sine(frequency: f32, time: f32) -> f32 {
-    (frequency * time * 2.0 * PI / SAMPLE_RATE).sin()
+fn sine(frequency: f32, time: f32, sample_rate: f32) -> f32 {
+    (frequency * time * 2.0 * PI / sample_rate).sin()
 }
 
-fn main() {
-    let mut sample_clock: f32 = 0.0;
-    let mut buffer: [f32; 1024] = [0.0; 1024];
+fn main() -> Result<(), failure::Error> {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("failed to find a default output device");
+    let format = device.default_output_format()?;
+    let event_loop = host.event_loop();
+    let stream_id = event_loop.build_output_stream(&device, &format)?;
+    event_loop.play_stream(stream_id.clone())?;
 
-    loop {
-        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
-        for index in 0..buffer.len() {
-            buffer[index] = sine(440.0, sample_clock);
+    let sample_rate = format.sample_rate.0 as f32;
+    let mut sample_clock = 0f32;
+
+    let mut next_value = || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        sine(440.0, sample_clock, sample_rate)
+    };
+
+    event_loop.run(move |id, result| {
+        let data = match result {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("an error occurred on stream {:?}: {}", id, err);
+                return;
+            }
+        };
+
+        match data {
+            cpal::StreamData::Output {
+                buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer),
+            } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            }
+            cpal::StreamData::Output {
+                buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer),
+            } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = (next_value() * std::i16::MAX as f32) as i16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            }
+            cpal::StreamData::Output {
+                buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer),
+            } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = next_value();
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            }
+            _ => (),
         }
-    }
+    });
 }
