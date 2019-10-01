7810492e9d48abd8e4d4234727a7d8e7a0935c9c
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2019-10-01 16:28:04 +0200

    Fill a buffer of samples with a simple sine wave

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::f32::consts::PI;
+
+const SAMPLE_RATE: f32 = 44_100.0;
+
+fn sine(frequency: f32, time: f32) -> f32 {
+    (frequency * time * 2.0 * PI / SAMPLE_RATE).sin()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut sample_clock: f32 = 0.0;
+    let mut buffer: [f32; 1024] = [0.0; 1024];
+
+    loop {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        for index in 0..buffer.len() {
+            buffer[index] = sine(440.0, sample_clock);
+        }
+    }
 }
