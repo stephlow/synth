a0dac9a1716456d2c7554966270eba5d6b02cdab
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2024-08-28 20:37:58 +0200

    Revitalize the project

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,150 @@
-extern crate coremidi;
-extern crate cpal;
-extern crate failure;
-extern crate wmidi;
+use std::sync::mpsc::{self, Receiver};
 
 use coremidi::{Client, PacketList};
-use oscillator::sine;
-use std::sync::mpsc;
-use wmidi::{MidiMessage, Note, Velocity};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    SizedSample,
+};
+use cpal::{FromSample, Sample};
+use oscillator::Oscillator;
+use wmidi::{MidiMessage, Note};
 
-use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
-use cpal::{StreamData, UnknownTypeOutputBuffer};
-
-mod gate;
 mod oscillator;
 
-use std::convert::TryFrom;
+static MIDI_CLIENT_NAME: &str = "example-client";
+static MIDI_DESTINATION_NAME: &str = "example-dest";
 
-#[derive(Default, Clone, Debug)]
-struct State {
-    voices: Vec<(Note, Velocity)>,
+pub enum MidiEvent {
+    NoteOn(Note),
+    NoteOff,
 }
 
-impl State {
-    fn add_voice(&mut self, note: Note, velocity: Velocity) {
-        self.voices.push((note, velocity));
-    }
-
-    fn remove_voice(&mut self, note: Note, velocity: Velocity) {
-        self.voices.retain(|(n, v)| !(n == &note && v == &velocity));
-    }
-}
-
-fn handle_midi_message(state: &mut State, bytes: &[u8]) {
+fn handle_midi_message(bytes: &[u8]) -> Option<MidiEvent> {
     if let Ok(message) = MidiMessage::try_from(bytes) {
         match message {
-            MidiMessage::NoteOn(_, note, velocity) => {
-                state.add_voice(note, velocity);
-            }
-            MidiMessage::NoteOff(_, note, velocity) => {
-                state.remove_voice(note, velocity);
-            }
-            _ => {}
+            MidiMessage::NoteOn(_, note, _velocity) => Some(MidiEvent::NoteOn(note)),
+            MidiMessage::NoteOff(_, _note, _velocity) => Some(MidiEvent::NoteOff),
+            _ => None,
         }
+    } else {
+        None
     }
 }
 
-fn main() -> Result<(), failure::Error> {
-    let mut state: State = Default::default();
+fn main() -> anyhow::Result<()> {
+    let (tx, rx) = mpsc::channel();
 
-    let (sender, receiver) = mpsc::channel();
+    let midi_client = Client::new(MIDI_CLIENT_NAME).unwrap();
 
-    let client = Client::new("example-client").unwrap();
-
+    let tx = tx.clone();
     let callback = move |packet_list: &PacketList| {
         for packet in packet_list.iter() {
-            handle_midi_message(&mut state, packet.data());
-            sender.send(state.clone()).unwrap();
+            if let Some(event) = handle_midi_message(packet.data()) {
+                tx.send(event).unwrap();
+            }
         }
     };
 
-    let _destination = client
-        .virtual_destination("example-dest", callback)
+    let _destination = midi_client
+        .virtual_destination(MIDI_DESTINATION_NAME, callback)
         .unwrap();
 
+    let stream = stream_setup_for(rx)?;
+    stream.play()?;
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(4000));
+    }
+
+    Ok(())
+}
+
+pub fn stream_setup_for(rx: Receiver<MidiEvent>) -> Result<cpal::Stream, anyhow::Error>
+where
+{
+    let (_host, device, config) = host_device_setup()?;
+
+    match config.sample_format() {
+        cpal::SampleFormat::I8 => make_stream::<i8>(&device, &config.into(), rx),
+        cpal::SampleFormat::I16 => make_stream::<i16>(&device, &config.into(), rx),
+        cpal::SampleFormat::I32 => make_stream::<i32>(&device, &config.into(), rx),
+        cpal::SampleFormat::I64 => make_stream::<i64>(&device, &config.into(), rx),
+        cpal::SampleFormat::U8 => make_stream::<u8>(&device, &config.into(), rx),
+        cpal::SampleFormat::U16 => make_stream::<u16>(&device, &config.into(), rx),
+        cpal::SampleFormat::U32 => make_stream::<u32>(&device, &config.into(), rx),
+        cpal::SampleFormat::U64 => make_stream::<u64>(&device, &config.into(), rx),
+        cpal::SampleFormat::F32 => make_stream::<f32>(&device, &config.into(), rx),
+        cpal::SampleFormat::F64 => make_stream::<f64>(&device, &config.into(), rx),
+        sample_format => Err(anyhow::Error::msg(format!(
+            "Unsupported sample format '{sample_format}'"
+        ))),
+    }
+}
+
+pub fn host_device_setup(
+) -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
     let host = cpal::default_host();
+
     let device = host
         .default_output_device()
-        .expect("failed to find a default output device");
-    let format = device.default_output_format()?;
-    let event_loop = host.event_loop();
-    let stream_id = event_loop.build_output_stream(&device, &format)?;
-    event_loop.play_stream(stream_id)?;
+        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
+    println!("Output device : {}", device.name()?);
 
-    let sample_rate = format.sample_rate.0 as f32;
-    let mut sample_clock = 0f32;
+    let config = device.default_output_config()?;
+    println!("Default output config : {:?}", config);
 
-    let mut next_value = |state: State| {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
+    Ok((host, device, config))
+}
 
-        match state.voices.first() {
-            Some((note, _frequency)) => sine(note.to_freq_f32(), sample_clock, sample_rate),
-            _ => 0.0,
-        }
-    };
+pub fn make_stream<T>(
+    device: &cpal::Device,
+    config: &cpal::StreamConfig,
+    rx: Receiver<MidiEvent>,
+) -> Result<cpal::Stream, anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let num_channels = config.channels as usize;
+    let mut oscillator = Oscillator::new(config.sample_rate.0 as f32);
+    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
 
-    event_loop.run(move |id, result| {
-        let mut state: State = Default::default();
+    let time_at_start = std::time::Instant::now();
+    println!("Time at start: {:?}", time_at_start);
 
-        if let Ok(new) = receiver.try_recv() {
-            state = new;
-        }
-
-        let data = match result {
-            Ok(data) => data,
-            Err(err) => {
-                eprintln!("an error occurred on stream {:?}: {}", id, err);
-                return;
-            }
-        };
-
-        match data {
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::U16(mut buffer),
-            } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    for out in sample.iter_mut() {
-                        *out = next_value(state.clone()) as u16;
+    let stream = device.build_output_stream(
+        config,
+        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
+            if let Ok(midi_event) = rx.try_recv() {
+                match midi_event {
+                    MidiEvent::NoteOn(note) => {
+                        oscillator.note_on(note.to_freq_f32());
+                    }
+                    MidiEvent::NoteOff => {
+                        oscillator.note_off();
                     }
                 }
             }
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::I16(mut buffer),
-            } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    for out in sample.iter_mut() {
-                        *out = next_value(state.clone()) as i16;
-                    }
-                }
-            }
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::F32(mut buffer),
-            } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    for out in sample.iter_mut() {
-                        *out = next_value(state.clone()) as f32;
-                    }
-                }
-            }
-            _ => (),
+            process_frame(output, &mut oscillator, num_channels)
+        },
+        err_fn,
+        None,
+    )?;
+
+    Ok(stream)
+}
+
+fn process_frame<SampleType>(
+    output: &mut [SampleType],
+    oscillator: &mut Oscillator,
+    num_channels: usize,
+) where
+    SampleType: Sample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(num_channels) {
+        let value: SampleType = SampleType::from_sample(oscillator.tick());
+
+        for sample in frame.iter_mut() {
+            *sample = value;
         }
-    });
+    }
 }
--- src/oscillator.rs
+++ src/oscillator.rs
@@ -1,5 +1,102 @@
-use std::f32::consts::PI;
+pub struct Oscillator {
+    pub sample_rate: f32,
+    pub waveform: Waveform,
+    pub current_sample_index: f32,
+    pub frequency_hz: f32,
+    pub gate: Gate,
+}
 
-pub fn sine(frequency: f32, time: f32, sample_rate: f32) -> f32 {
-    (frequency * time * 2.0 * PI / sample_rate).sin()
+pub enum Waveform {
+    Sine,
+    Square,
+    Saw,
+    Triangle,
+}
+
+pub enum Gate {
+    High,
+    Low,
+}
+
+impl Oscillator {
+    pub fn new(sample_rate: f32) -> Self {
+        Self {
+            waveform: Waveform::Sine,
+            sample_rate,
+            current_sample_index: 0.0,
+            frequency_hz: 440.0,
+            gate: Gate::Low,
+        }
+    }
+
+    pub fn note_on(&mut self, freq: f32) {
+        self.frequency_hz = freq;
+        self.gate = Gate::High;
+    }
+
+    pub fn note_off(&mut self) {
+        self.gate = Gate::Low;
+    }
+
+    pub fn set_frequency(&mut self, frequency_hz: f32) {
+        self.frequency_hz = frequency_hz;
+    }
+
+    fn advance_sample(&mut self) {
+        self.current_sample_index = (self.current_sample_index + 1.0) % self.sample_rate;
+    }
+
+    pub fn set_waveform(&mut self, waveform: Waveform) {
+        self.waveform = waveform;
+    }
+
+    fn calculate_sine_output_from_freq(&self, freq: f32) -> f32 {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        (self.current_sample_index * freq * two_pi / self.sample_rate).sin()
+    }
+
+    fn is_multiple_of_freq_above_nyquist(&self, multiple: f32) -> bool {
+        self.frequency_hz * multiple > self.sample_rate / 2.0
+    }
+
+    fn sine_wave(&mut self) -> f32 {
+        self.advance_sample();
+        self.calculate_sine_output_from_freq(self.frequency_hz)
+    }
+
+    fn generative_waveform(&mut self, harmonic_index_increment: i32, gain_exponent: f32) -> f32 {
+        self.advance_sample();
+        let mut output = 0.0;
+        let mut i = 1;
+        while !self.is_multiple_of_freq_above_nyquist(i as f32) {
+            let gain = 1.0 / (i as f32).powf(gain_exponent);
+            output += gain * self.calculate_sine_output_from_freq(self.frequency_hz * i as f32);
+            i += harmonic_index_increment;
+        }
+        output
+    }
+
+    fn square_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 1.0)
+    }
+
+    fn saw_wave(&mut self) -> f32 {
+        self.generative_waveform(1, 1.0)
+    }
+
+    fn triangle_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 2.0)
+    }
+
+    pub fn tick(&mut self) -> f32 {
+        match self.gate {
+            Gate::High => match self.waveform {
+                Waveform::Sine => self.sine_wave(),
+                Waveform::Square => self.square_wave(),
+                Waveform::Saw => self.saw_wave(),
+                Waveform::Triangle => self.triangle_wave(),
+            },
+            Gate::Low => 0.,
+        }
+    }
 }
